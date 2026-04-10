5e36b1a09254f6e6e41de280c048bdc852ead35b
PhilipGamer <122830732+philipgamer@users.noreply.github.com>
2026-04-10 14:22:10 +0200

    Source code added

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Minecraft_server_helfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(41, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(484, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Hey! ich bin dein Minecraft server helfer!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(31, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(426, 32);
+            label2.TabIndex = 0;
+            label2.Text = "Sag mir wie soll dein Server heißen?";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ButtonHighlight;
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(31, 100);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(261, 39);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Hier eingeben!";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(31, 164);
+            label3.Name = "label3";
+            label3.Size = new Size(430, 64);
+            label3.TabIndex = 0;
+            label3.Text = "Der Minecraft server wird \r\nstandardmäßig mit 8GB ram erstellt.";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(31, 228);
+            label4.Name = "label4";
+            label4.Size = new Size(495, 96);
+            label4.TabIndex = 0;
+            label4.Text = "willst du das ändern. musst du die \r\n\"start.bat.\" bearbeiten. und den wert\r\nbei -Xmx8G ändern auf deine wunschwert";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(31, 375);
+            label5.Name = "label5";
+            label5.Size = new Size(475, 64);
+            label5.TabIndex = 0;
+            label5.Text = "Klick jetzt einfach auf \"Server erstellen\" \r\ndu musst nichts mehr machen! ";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(165, 453);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 41);
+            button1.TabIndex = 2;
+            button1.Text = "Server erstellen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ControlDark;
+            label6.Location = new Point(0, 623);
+            label6.Name = "label6";
+            label6.Size = new Size(143, 20);
+            label6.TabIndex = 3;
+            label6.Text = "© 2026 PhilipGamer";
+            label6.Click += label6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(565, 644);
+            Controls.Add(label6);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Minecraft Server Helfer";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Label label6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Minecraft_server_helfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // 1. Name aus der TextBox holen
+                string serverName = textBox1.Text;
+
+                // Falls das Feld leer ist, geben wir eine Warnung aus
+                if (string.IsNullOrWhiteSpace(serverName))
+                {
+                    MessageBox.Show("Bitte gib einen Namen für deinen Server ein!");
+                    return;
+                }
+
+                // 2. Pfad auf dem Desktop festlegen
+                string pfad = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), serverName);
+
+                // 3. Ordner erstellen
+                if (!Directory.Exists(pfad))
+                {
+                    Directory.CreateDirectory(pfad);
+                }
+
+                // 4. Die eula.txt automatisch erstellen (Spart dem User Schritt 4!)
+                File.WriteAllText(Path.Combine(pfad, "eula.txt"), "eula=true");
+
+                // 5. Die start.bat mit 8GB RAM erstellen
+                string batchInhalt = $"@echo off\ntitle {serverName}\njava -Xms8G -Xmx8G -jar paper.jar nogui\npause";
+                File.WriteAllText(Path.Combine(pfad, "start.bat"), batchInhalt);
+
+                // 6. Die Download-Seite für die paper.jar öffnen
+                // (Später können wir das durch einen automatischen Download ersetzen!)
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://papermc.io/downloads/paper",
+                    UseShellExecute = true
+                });
+
+                // 7. Erfolg melden
+                MessageBox.Show("Erfolg! Der Server-Ordner wurde auf deinem Desktop vorbereitet.\n\n" +
+                                "Nächste Schritte:\n" +
+                                "1. Lade die Server-Datei von der Webseite herunter.\n" +
+                                "2. Verschiebe sie in den neuen Ordner.\n" +
+                                "3. Benenne die Datei exakt in 'paper.jar' um.\n\n" +
+                                "Danach kannst du die start.bat starten!",
+                                "Server Vorbereitung",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Da ist etwas schiefgelaufen: " + ex.Message);
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/philipgamer/Minecraft-Server-helfer?tab=MIT-1-ov-file") { UseShellExecute = true });
+        }
+    }
+}
